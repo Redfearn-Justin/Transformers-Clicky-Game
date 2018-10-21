9ce044a29c8db68d2d1c97b0dc5e4d860dc85608
Napoleon <wanderofrazgriz@gmail.com>
2018-10-21 00:09:36 -0700

    static page successfully rendered:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TransformersCard from "./components/transformer_card";
+import Wrapper from "./components/wrapper";
+import Title from "./components/title";
+import transformers from "./transformers.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    transformers
+  };
+
+  // removeFriend = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const friends = this.state.friends.filter(friend => friend.id !== id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ friends });
+  // };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Title>Battle For Cybertron</Title>
+        {this.state.transformers.map(transformers => (
+          <TransformersCard
+            removeFriend={this.removeFriend}
+            id={transformers.id}
+            key={transformers.id}
+            name={transformers.name}
+            image={transformers.image}
+            role={transformers.role}
+            altmode={transformers.altmode}
+            affiliation={transformers.affiliation}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
